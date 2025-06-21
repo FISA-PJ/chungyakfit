9ac980a3b590551a781cc11db1ea871bf4b14793
leetaemin <ltm7149@konkuk.ac.kr>
2025-06-21 21:00:51 +0900

    Add Backend file

---
+++ ml-backend/src/main/java/com/mysite/applyhome/ApplyHomeApplication.java
@@ -0,0 +1,14 @@
+package com.mysite.applyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyHomeApplication.class, args);
+	}
+
+}
